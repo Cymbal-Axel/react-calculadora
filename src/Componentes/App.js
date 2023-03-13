ef1f3209197f941d74292a7c5c07aee669bbeea4
Axel <cymbalaxel@gmail.com>
2023-03-13 17:28:46 -0300

    se agrego la carpeta componentes

---
+++ src/Componentes/App.js
@@ -0,0 +1,15 @@
+import Display from "./Display";
+import PanelDeBotones from "./PanelDeBotones";
+import {Component} from "react";
+
+class App extends Component() {
+  render(){
+    return (
+      <div className="App">
+        <p>todo va bien!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
