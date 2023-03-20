16f3df88f04e98b67e76d0be7672548f094f341b
Axel <cymbalaxel@gmail.com>
2023-03-20 17:44:45 -0300

    se agregaron botones

--- src/Componentes/PanelDeBotones.js
+++ src/Componentes/PanelDeBotones.js
@@ -12,6 +12,24 @@ export default function PanelDeBotones(clickHandle){
                 <Button name="%" clickHandle={handleClick}/>
                 <Button name="/" clickHandle={handleClick}/>
             </div>
+            <div>
+                <Button name="7" clickHandle={handleClick}/>
+                <Button name="8" clickHandle={handleClick}/>
+                <Button name="9" clickHandle={handleClick}/>
+                <Button name="X" clickHandle={handleClick}/>
+            </div>
+            <div>
+                <Button name="4" clickHandle={handleClick}/>
+                <Button name="5" clickHandle={handleClick}/>
+                <Button name="6" clickHandle={handleClick}/>
+                <Button name="-" clickHandle={handleClick}/>
+            </div>
+            <div>
+                <Button name="1" clickHandle={handleClick}/>
+                <Button name="2" clickHandle={handleClick}/>
+                <Button name="3" clickHandle={handleClick}/>
+                <Button name="+" clickHandle={handleClick}/>
+            </div>
         </div>
     )
 
